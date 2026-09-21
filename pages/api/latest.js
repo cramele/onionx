0015cb52557e2c3949bccff3a528550377d8cf14
cramele <89088281+cramele@users.noreply.github.com>
2026-09-21 12:54:05 +0300

    Update latest.js

--- pages/api/latest.js
+++ pages/api/latest.js
@@ -1,7 +1,6 @@
 // pages/api/latest.js
 export default async function handler(req, res) {
-  const token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpYXQiOjE3ODk5ODQyNDksInJvbGVzIjpbIlJPTEVfVVNFUiJdLCJhZGRyZXNzIjoic2tpbmFwZUB1YmVyaXAuY29tIiwiaWQiOiI2YWIwZmRlYjQ0MjE0M2I5YjQwYmRhZjAiLCJtZXJjdXJlIjp7InN1YnNjcmliZSI6WyIvYWNjb3VudHMvNmFiMGZkZWI0NDIxNDNiOWI0MGJkYWYwIl19fQ.CQxtHzHRSqPgUvZWsv_KJod7JER0Uy80xLwBAPxvkgyC-iyaUAMc8VE2sY8aoD5_jilVS7vWOCdTmPH7SqxWxw
-"; // your full token here
+  const token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpYXQiOjE3ODk5ODQyNDksInJvbGVzIjpbIlJPTEVfVVNFUiJdLCJhZGRyZXNzIjoic2tpbmFwZUB1YmVyaXAuY29tIiwiaWQiOiI2YWIwZmRlYjQ0MjE0M2I5YjQwYmRhZjAiLCJtZXJjdXJlIjp7InN1YnNjcmliZSI6WyIvYWNjb3VudHMvNmFiMGZkZWI0NDIxNDNiOWI0MGJkYWYwIl19fQ.CQxtHzHRSqPgUvZWsv_KJod7JER0Uy80xLwBAPxvkgyC-iyaUAMc8VE2sY8aoD5_jilVS7vWOCdTmPH7SqxWxw"; // your full token here
 
   const messagesRes = await fetch("https://api.mail.tm/messages?page=1&limit=5", {
     headers: { Authorization: `Bearer ${token}` }
